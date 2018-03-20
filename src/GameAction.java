b0d787ee873c7240c2f400f8565d44ca241aaf22
He Chen <chenhe95@hotmail.com>
2018-03-19 20:32:40 -0400

    init

---
+++ src/GameAction.java
@@ -0,0 +1,4 @@
+
+public enum GameAction {
+	ACTION_1, ACTION_2, ACTION_3, ACTION_NULL
+}
---
+++ src/GameState.java
@@ -0,0 +1,11 @@
+
+public class GameState {
+	private int[] data = null;
+	public GameState(int[] data) {
+		this.data = data;
+	}
+	
+	public int[] getData() {
+		return data;
+	}
+}
