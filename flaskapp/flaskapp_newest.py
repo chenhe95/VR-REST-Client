830cae6e358f875ff9587b9fef2d0a5d89d197cf
chenhe95 <chenhe95@hotmail.com>
2018-04-11 02:20:52 -0400

    .

--- flaskapp/flaskapp_newest.py
+++ flaskapp/flaskapp_newest.py
@@ -1,13 +1,16 @@
 # 1.1
 
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 import pickle
+import traceback
+import pandas as pd
 import numpy as np
-
+import os
 import sklearn
-
-print sklearn.__version__
-print np.__version__
+from io import StringIO
+from werkzeug.utils import secure_filename
+# print sklearn.__version__
+# print np.__version__
 
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import CategoricalEncoder
@@ -18,7 +21,7 @@ import MySQLdb
 ALLOWED_EXTENSIONS = ["csv"]
 
 app = Flask(__name__)
-
+app.debug = True
 app.config['UPLOAD_FOLDER'] = "upload_folder"
 
 clf = None
@@ -35,8 +38,10 @@ settings_db = [None for i in xrange(6)]
 with app.open_resource("settings.db", "r") as f_in:
     i = 0
     for line in f_in:
-        settings_db[i] = line
+        settings_db[i] = line.strip()
         i += 1
+        if i >= 6:
+            break
 
 db_driver = settings_db[0]
 db_db = settings_db[1]
@@ -69,8 +74,11 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def train_decision_maker(file_instance, file_name):
-    data_pd = pd.read_csv(file_instance)
 
+    print "initiating training from file"
+    
+    data_pd = pd.read_csv(StringIO(unicode(file_instance.read())), header=0)
+    print "read file"
     clf = LinearSVC()
 
     data_pd
@@ -92,39 +100,54 @@ def train_decision_maker(file_instance, file_name):
 
     clf.predict(X)
 
-    with open(os.path.join(app.config['UPLOAD_FOLDER'], file_name + ".pickle", "w")) as f_out:
+    print "fitted file"
+    
+    with open(os.path.join("/home", "ubuntu", "flaskapp", app.config['UPLOAD_FOLDER'], file_name), "w") as f_out:
         pickle.dump(clf, f_out)
 
+    print "saved file"
+
 def load_decision_maker(file_name):
-    with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'], file_name, "r")) as f_in:
+    with app.open_resource(os.path.join(app.config['UPLOAD_FOLDER'], file_name), "r") as f_in:
         clf = pickle.load(f_in)
 
         return clf
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/submit_behavior/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    print "uploading file"
+    print request
+    try:
+        if request.method == 'POST':
+            print "post received"
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                print "good filename"
+                filename = secure_filename(file.filename)
+                print filename
+                # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                print "saved file"
+                train_decision_maker(file, filename)
 
-            train_decision_maker(file, filename)
-
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+                return "Behavior uploaded"
+        else:
+            print "not post"    
+    except Exception as e:
+        print "exception occured", str(e)
+        traceback.print_exc()
+        
     return '''
     <!doctype html>
-    <title>Upload new File</title>
+    <title>Upload AI Behavior</title>
     <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
       <p><input type=file name=file>
@@ -133,8 +156,8 @@ def upload_file():
     '''
 
 @app.route('/get_decision_behavior/<behavior_file_name>/<game_state>')
-def get_decision(behavior_file_name, game_state):
-    global clf
+def get_decision_behavior(behavior_file_name, game_state):
+    clf = None
     try:
         if clf is None:
             clf = load_decision_maker(behavior_file_name)
